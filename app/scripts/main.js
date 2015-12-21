ee5c99686146055968cac7db5af3217e76aebf56
Emory Griffith <emorygriffith91@gmail.com>
2015-12-21 15:28:39 -0500

    Initial Commit - setup basic app structure and dependencies

---
+++ app/scripts/main.js
@@ -0,0 +1,7 @@
+var data = {
+    event1: {start: 60, end: 120},  // an event from 10am to 11am
+    event2: {start: 100, end: 240}, // an event from 10:40am to 1pm
+    event3: {start: 700, end: 720}  // an event from 8:40pm to 9pm
+};
+
+console.log(data);
