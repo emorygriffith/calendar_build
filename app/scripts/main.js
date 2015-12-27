7568fdc85ed4ddabe72e2ba66eb068e0aad01c5a
Emory Griffith <emorygriffith91@gmail.com>
2015-12-27 17:06:07 -0500

    now it's setting the attributes niside the loop... yay we're onto something

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,28 +5,50 @@ var data = {
     event3: {start: 700, end: 720}  // an event from 8:40pm to 9pm
 };
 
+//Create Event Model
+var Event = Backbone.Model.extend({
+  defaults: {
+    top: 0,
+    left: 0
+  },
 
-var y = $.each(data, function(){
-
-    var top = this.end - this.start;
-    var left = 0;
-
-    var Event = Backbone.Model.extend({
-      defaults: {
-        top: top,
-        left: left
-      },
-
-      initialize: function(){
-              var t = this.get("top");
-              var l = this.get("left");
-              console.log("A new Event has been added. It has the following properties: Top: " + t + ", Left: " + l);
-            },
-    });
-
-    var x = new Event();
-    console.log(x);
-
+  initialize: function(){
+          var t = this.get("top");
+          var l = this.get("left");
+          //console.log("A new Event has been added. It has the following properties: Top: " + t + ", Left: " + l);
+        },
 });
 
-console.log(y);
+
+//Setup EventCollection
+var EventCollection = Backbone.Collection.extend({
+    model: Event
+});
+
+
+//Instantiate the Events Collection
+var Events = new EventCollection;
+
+
+//loop through JSON data and add objects the events collection
+$.each(data, function(){
+
+  var event = new Event();
+
+  var the_top = this.end - this.start;
+  var the_left = 0;
+
+  event.set("top",the_top);
+  event.set("left", the_left);
+
+  console.log(event);
+
+
+
+
+
+
+
+
+
+});
