bcaf023878c81080072f6a9262e692b1c0a3a68c
Emory Griffith <emorygriffith91@gmail.com>
2015-12-27 14:30:50 -0500

    setup backbone model for event

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,46 +1,36 @@
-var events = {
+//JSON Data
+var data = {
     event1: {start: 60, end: 120},  // an event from 10am to 11am
     event2: {start: 100, end: 240}, // an event from 10:40am to 1pm
     event3: {start: 700, end: 720}  // an event from 8:40pm to 9pm
 };
 
+$.each(data, function(){
 
-  $('<div class=\'box\' style=\'width:100%;height:40px;\'/>').appendTo('.cal-container');
-
-  $.each(events, function(key, value){
-    var height = value.end - value.start;
-    console.log(height);
-
-    function createDiv(height){
-
-    $('<div class=\'box\' style=\'width:100%;height:'+height+'px;\'/>').appendTo('.cal-container');
-
-}
+    var start = this.start;
+    var end = this.end;
+    var height = this.end - this.start;
+    var a = 35;
+    console.log(start, end, height);
 
 
-  });
+    //setup the Backbone Model
+    var Event = Backbone.Model.extend({
+      defaults: {
+        start: start,
+        end: end,
+        height: height
+      },
+
+      initialize: function(){
+              var t = this.get("start");
+              console.log(t + " has been added");
+            },
+
+    });
 
 
 
+new Event(); //instantiate each model
 
-  // $( "div.example" ).css( "width", function( index ) {
-  // return index * 50;
-// });
-
-
-
-
-// });
-
-// //  $( ".cal-container" ).add( "event" ).css( "background-color", "red" );
-//
-//
-//
-//
-//
-//
-//
-// //
-//   //$(".cal-container").css("background-color", "yellow");
-//
-//
+}); //end $.each loop
